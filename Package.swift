637dd4d37a17e37ebeb70eb75b3f1aa0ad812db4
Jack Maloney <git@jmmaloney4.xyz>
2021-09-20 17:09:22 -0500

    Refactor to Multiformat.swift

--- Package.swift
+++ Package.swift
@@ -4,12 +4,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "CID",
+    name: "Multiformat",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "CID",
-            targets: ["CID"]
+            name: "Multiformat",
+            targets: ["Multiformat"]
         ),
     ],
     dependencies: [
@@ -22,12 +22,12 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "CID",
+            name: "Multiformat",
             dependencies: ["VarInt", "Multihash"]
         ),
         .testTarget(
-            name: "CIDTests",
-            dependencies: ["CID"]
+            name: "MultiformatTests",
+            dependencies: ["Multiformat"]
         ),
     ]
 )
--- Tests/MultiformatTests/CIDTests.swift
+++ Tests/MultiformatTests/CIDTests.swift
@@ -5,7 +5,7 @@
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 import XCTest
-@testable import CID
+@testable import Multiformat
 
 final class CIDTests: XCTestCase {
     func testExample() throws {
