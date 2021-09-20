6a28d38ac2fb980bc52cbb17b9b527eeec5f78c2
Jack Maloney <git@jmmaloney4.xyz>
2021-09-20 14:00:54 -0500

    Refactor and add VarInt

--- Package.swift
+++ Package.swift
@@ -4,25 +4,28 @@
 import PackageDescription
 
 let package = Package(
-    name: "CID.swift",
+    name: "CID",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "CID.swift",
-            targets: ["CID.swift"]),
+            name: "CID",
+            targets: ["CID"]
+        ),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/jmmaloney4/VarInt.git", .branch("master")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "CID.swift",
-            dependencies: []),
+            name: "CID",
+            dependencies: ["VarInt"]
+        ),
         .testTarget(
-            name: "CID.swiftTests",
-            dependencies: ["CID.swift"]),
+            name: "CIDTests",
+            dependencies: ["CID"]
+        ),
     ]
 )
--- Sources/CID.swift/CID_swift.swift
+++
@@ -1,3 +0,0 @@
-struct CID_swift {
-    var text = "Hello, World!"
-}
