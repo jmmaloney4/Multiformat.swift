8555dd428f03e058b268c2cd395ca8a346e36d16
Jack Maloney <git@jmmaloney4.xyz>
2021-09-29 16:19:07 -0500

    Add LinuxMain.swift

---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,12 @@
+// Copyright © 2021 Jack Maloney. All Rights Reserved.
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+import XCTest
+@testable import MultiformatTests
+
+XCTMain([
+    testCase(MultiformatTests.allTests),
+])
