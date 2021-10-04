a567fa7c6e33d61c38ad1d687736343d947b831f
Jack Maloney <jmmaloney4@gmail.com>
2021-10-04 10:51:53 -0500

    Update LinuxMain.swift

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -4,9 +4,8 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-import XCTest
-@testable import MultiformatTests
-
-XCTMain([
-    testCase(MultiformatTests.allTests),
-])
+#error("""
+  -----------------------------------------------------
+  Please test with `swift test --enable-test-discovery`
+  -----------------------------------------------------
+  """)
